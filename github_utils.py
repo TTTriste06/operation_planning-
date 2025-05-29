af236a8a7f48fac917955c48ab2603d32b352ea3
TTTriste06 <yunhan.yang08@gmail.com>
2025-05-29 13:50:52 +0800

    Update github_utils.py

--- github_utils.py
+++ github_utils.py
@@ -7,7 +7,7 @@ from urllib.parse import quote
 
 # GitHub 配置
 GITHUB_TOKEN_KEY = "GITHUB_TOKEN"  # secrets.toml 中的密钥名
-REPO_NAME = "TTTriste06/semiexcel"
+REPO_NAME = "TTTriste06/operation_planning-"
 BRANCH = "main"
 
 FILE_RENAME_MAPPING = {
