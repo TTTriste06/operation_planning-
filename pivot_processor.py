2d4c64a622537c443556d1934f9724f677ab5ff9
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-03 11:31:16 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -70,7 +70,6 @@ class PivotProcessor:
         # 新建主计划df
         headers = ["晶圆品名", "规格", "品名", "封装厂", "封装形式", "pc"]
         main_plan_df = pd.DataFrame(columns=headers)
-        main_plan_df = main_plan_df.reindex(index=range(len(all_names))) # 用 reindex 来匹配品名数量（行数）
 
         # 品名：提取未交订单和预测中的品名
         name_unfulfilled = []
@@ -90,8 +89,8 @@ class PivotProcessor:
         all_names = pd.Series(name_unfulfilled + name_forecast).dropna().drop_duplicates().sort_values()
     
         # 正确构造空 DataFrame 并填入“品名”列
+        main_plan_df = main_plan_df.reindex(index=range(len(all_names))) # 用 reindex 来匹配品名数量（行数）
         main_plan_df = pd.DataFrame({col: None for col in headers})  # 创建结构
-        main_plan_df = main_plan_df.reindex(index=range(len(all_names)))  # 匹配行数
         main_plan_df["品名"] = all_names.values  # 仅填“品名”
 
 
