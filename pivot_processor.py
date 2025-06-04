cdd2d30f0857c1e83a0ee67a89ae255d903217ac
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-04 15:59:02 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -225,7 +225,8 @@ class PivotProcessor:
             merge_inventory_header(ws)
             merge_product_in_progress_header(ws)
 
-
+            apply_monthly_grouped_headers(ws)
+            
             adjust_column_width(ws)
 
         output_buffer.seek(0)
