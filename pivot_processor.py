e25d670b52bf5c10efe9bd3375d6120642d2ea43
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-11 10:42:45 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -117,13 +117,13 @@ class PivotProcessor:
 
         ## == 替换新旧料号、替代料号 ==
         st.write(self.additional_sheets["赛卓-安全库存"])
-        st.write(additional_sheets["赛卓-安全库存"])
         df_new = self.additional_sheets["赛卓-安全库存"]
-        
+        st.write(self.additional_sheets["赛卓-安全库存"])
         df_new, _ = apply_mapping_and_merge(df_new, mapping_df, FIELD_MAPPINGS["赛卓-安全库存"])
-
+        st.write(self.additional_sheets["赛卓-安全库存"])
         # df_new, _ = apply_extended_substitute_mapping(df_new, mapping_df, FIELD_MAPPINGS["赛卓-安全库存"])
         self.additional_sheets["赛卓-安全库存"] = df_new
+        st.write(self.additional_sheets["赛卓-安全库存"])
         
         df_new = self.additional_sheets["赛卓-预测"]
         df_new, _ = apply_mapping_and_merge(df_new, mapping_df, FIELD_MAPPINGS["赛卓-预测"])
