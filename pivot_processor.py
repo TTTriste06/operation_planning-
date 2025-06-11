86b5233b95ee42361f2017cff4979cf128136e95
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-11 13:12:53 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -92,7 +92,6 @@ class PivotProcessor:
         st.write(mapping_semi)
         st.write(mapping_new)
         st.write(mapping_sub)
-        #st.write(mapping_sub["替代品名2"].unique()）
 
         # === 构建主计划 ===
         headers = ["晶圆品名", "规格", "品名", "封装厂", "封装形式", "PC"]
