505e5118834220d59a76f273df02f0c781a42caa
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-25 13:26:28 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -415,7 +415,7 @@ class PivotProcessor:
             ws_diagram = wb["Summary"]
             
             # 添加一个文档内部超链接
-            cell1 = ws_diagram.cell(row=2, column=2)  ✅（正确）
+            cell1 = ws_diagram.cell(row=2, column=2)
             internalLink = ws_diagram.HyperLinks.Add(cell1)
             internalLink.Type = HyperLinkType.Workbook
             internalLink.TextToDisplay = "主计划"
