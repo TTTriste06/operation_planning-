06719637414417814232f780963e419a26c1499d
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-25 09:50:53 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -18,7 +18,7 @@ def get_uploaded_files():
     st.header("📤 Excel 数据处理与汇总")
 
     # 📅 输入历史截止月份
-    manual_month = st.text_input("📅 输入历史数据截止月份（格式: YYYY-MM，可留空表示不筛选）")
+    # manual_month = st.text_input("📅 输入历史数据截止月份（格式: YYYY-MM，可留空表示不筛选）")
     # CONFIG["selected_month"] = manual_month.strip() if manual_month.strip() else None
 
     # 📅 添加主计划起始时间选择器
@@ -28,11 +28,10 @@ def get_uploaded_files():
         value=datetime(datetime.now().year, datetime.now().month, 1)  # 默认选当月1号
     )
 
-
-
     # ✅ 合并上传框：所有主+明细文件统一上传
+    st.subheader("📁 上传主文件")
     all_files = st.file_uploader(
-        "📁 上传主文件: 未交订单/成品在制/成品库存/CP在制/晶圆库存/下单明细/销货明细/到货明细（支持多选）",
+        "未交订单/成品在制/成品库存/CP在制/晶圆库存/下单明细/销货明细/到货明细（支持多选）",
         type=["xlsx"],
         accept_multiple_files=True,
         key="all_files"
