0bb24598435c52a4ad1e62ff50312fbc746a2eab
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-25 14:15:41 +0800

    Update production_plan.py

--- production_plan.py
+++ production_plan.py
@@ -112,9 +112,10 @@ def generate_monthly_fg_plan(main_plan_df: pd.DataFrame, forecast_months: list[i
                 v_prev_plan = get_plan(col_target_prev).at[row_idx]
                 v_actual = get(col_actual_prod).at[row_idx]
                 v_forecast_next = get(col_forecast_next).at[row_idx]
+                v_order_next = get(col_order_next).at[row_idx]
 
                 formula = f"{v_prev_plan} - {v_actual} + {v_forecast_next}"
-                result = v_prev_plan + max_next
+                result = v_prev_plan + max(v_forecast_next, v_order_next)
                 df_plan.at[row_idx, col_target] = result
                 
     plan_cols_in_summary = [col for col in main_plan_df.columns if "成品投单计划" in col and "半成品" not in col]
