e2c157d0b3afe5a1a13fdbe618b50cb909347766
TTTriste06 <yunhan.yang08@gmail.com>
2025-06-25 13:23:47 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from openpyxl.utils import get_column_letter
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Alignment, PatternFill
+from spire.xls import Workbook, HyperLinkType, FileFormat
 
 from config import FILE_KEYWORDS, OUTPUT_FILENAME_PREFIX, FIELD_MAPPINGS, pivot_config, RENAME_MAP
 from excel_utils import (
