d51e94eccf54e729defb3c0abfd4cb51c67aaca9
TTTriste06 <yunhan.yang08@gmail.com>
2025-07-03 17:23:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
         buffer = BytesIO()
         processor = PivotProcessor()
         processor.set_additional_data(additional_sheets)
-        processor.process(uploaded_files, buffer, additional_sheets, uploaded_cp_files, start_date=selected_date)
+        processor.process(uploaded_files, uploaded_cp_files, buffer, additional_sheets, start_date=selected_date)
 
         # 下载文件按钮
         file_name = f"运营数据订单-在制-库存汇总报告_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"
