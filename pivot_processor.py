c488c2facde75b64ee00a7a1a4839ce1483f6bf9
TTTriste06 <yunhan.yang08@gmail.com>
2025-07-10 09:48:25 +0800

    Update pivot_processor.py

--- pivot_processor.py
+++ pivot_processor.py
@@ -296,7 +296,6 @@ class PivotProcessor:
         df_unique_wafer = append_fab_warehouse_quantity(df_unique_wafer, self.SH_fabout)
         df_unique_wafer = append_monthly_wo_from_weekly_fab(df_unique_wafer, df_fab_summary)
 
-        st.write(main_plan_df)
         df_unique_wafer = append_monthly_demand_from_fg_plan(df_unique_wafer, main_plan_df)
 
         
